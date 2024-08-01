b50ce78215fd0691cb0e24443d1fb056b9a1b83e
Alben-cmd <alenoghenabenjamin@gmail.com>
2024-08-02 00:21:01 +0100

    product crud

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +24,12 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/user', [AuthController::class, 'getUser']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+    Route::put('/product/{id}/edit', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/product/{id}/delete', [ProductController::class, 'destroy'])->name('product.destroy');
+
 
 });
