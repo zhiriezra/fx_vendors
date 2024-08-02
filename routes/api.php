ff692db66709fc5c99b9664eadd42446e6ad7ca0
Alben-cmd <alenoghenabenjamin@gmail.com>
2024-08-02 23:30:47 +0100

    image intervention

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/product', [ProductController::class, 'index'])->name('product.index');
     Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+    Route::post('/{id}/addImage', [ProductController::class, 'addImage'])->name('product.addImage');
     Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
     Route::put('/product/{id}/edit', [ProductController::class, 'update'])->name('product.update');
     Route::delete('/product/{id}/delete', [ProductController::class, 'destroy'])->name('product.destroy');
