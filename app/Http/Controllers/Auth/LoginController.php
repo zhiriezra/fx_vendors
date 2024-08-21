63407433ebf97ba09a3e27ea4348e0bcf624d9b9
Alben-cmd <alenoghenabenjamin@gmail.com>
2024-08-21 17:58:41 +0100

    order clean up

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,11 @@ class LoginController extends Controller
      */
     protected $redirectTo = '/vendor/dashboard';
 
+    protected function authenticated(Request $request, $user)
+    {
+        session()->flash('welcome_message', "Welcome back, {$user->firstname}! 👋");
+        return redirect()->intended('/vendor/dashboard');
+    }
     /**
      * Create a new controller instance.
      *
