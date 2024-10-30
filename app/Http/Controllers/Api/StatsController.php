c7cb3be1b38d3f3a08f99c8d36f8f5468ede533a
zhiriezra <zhiriezra@gmail.com>
2024-10-30 06:43:13 +0100

    updated api for Product Upload to include image

--- app/Http/Controllers/Api/StatsController.php
+++ app/Http/Controllers/Api/StatsController.php
@@ -8,65 +8,6 @@ use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
-    public function supplied(){
-
-        $vendor = auth()->user()->vendor;
-
-        if($vendor){
-            $totalSuppliedOrders = $vendor->products()->withCount(['orders' => function ($query){
-                $query->where('status', 'confirmed');
-            }])->get()->sum('orders_count');
-
-            return response()->json(['status' => true, 'message' => 'Total supplied order(s)', 'data' => ['total' => $totalSuppliedOrders]]);
-        }
-
-        return response()->json(['status' => false, 'message' => 'Vendor not found']);
-    }
-
-    public function pending(){
-
-        $vendor = auth()->user()->vendor;
-
-        if($vendor){
-            $totalSuppliedOrders = $vendor->products()->withCount(['orders' => function ($query){
-                $query->where('status', 'pending');
-            }])->get()->sum('orders_count');
-
-            return response()->json(['status' => true, 'message' => 'Total pending order(s)', 'data' => ['total' => $totalSuppliedOrders]]);
-        }
-
-        return response()->json(['status' => false, 'message' => 'Vendor not found']);
-    }
-
-    public function accepted(){
-
-        $vendor = auth()->user()->vendor;
-
-        if($vendor){
-            $totalSuppliedOrders = $vendor->products()->withCount(['orders' => function ($query){
-                $query->where('status', 'accepted');
-            }])->get()->sum('orders_count');
-
-            return response()->json(['status' => true, 'message' => 'Total accepted order(s)', 'data' => ['total' => $totalSuppliedOrders]]);
-        }
-
-        return response()->json(['status' => false, 'message' => 'Vendor not found']);
-    }
-
-    public function total(){
-
-        $vendor = auth()->user()->vendor;
-
-        if($vendor){
-            $totalSuppliedOrders = $vendor->products()->withCount('orders')->get()->sum('orders_count');
-
-            return response()->json(['status' => true, 'message' => 'Total accepted order(s)', 'data' => ['total' => $totalSuppliedOrders]]);
-        }
-
-        return response()->json(['status' => false, 'message' => 'Vendor not found']);
-    }
-
-    //This should replace the above. One end point for all request should be better
 
     public function dashboardStats()
     {
@@ -80,6 +21,9 @@ class StatsController extends Controller
                         $query->where('status', 'confirmed');
                     },
                     'orders as pending_orders_count' => function ($query) {
+                        $query->where('status', 'supplied');
+                    },
+                    'orders as pending_orders_count' => function ($query) {
                         $query->where('status', 'pending');
                     },
                     'orders as accepted_orders_count' => function ($query) {
@@ -91,6 +35,7 @@ class StatsController extends Controller
 
 
             $totalConfirmedOrders = $products->sum('confirmed_orders_count');
+            $totalSuppliedOrders = $products->sum('confirmed_orders_count');
             $totalPendingOrders = $products->sum('pending_orders_count');
             $totalAcceptedOrders = $products->sum('accepted_orders_count');
             $totalOrders = $products->sum('orders_count');
@@ -99,15 +44,16 @@ class StatsController extends Controller
                 'status' => true,
                 'message' => 'Categories of Orders',
                 'data' => [
-                    'totalConfirmedOrders' => $totalConfirmedOrders,
-                    'totalPendingOrders' => $totalPendingOrders,
-                    'totalAcceptedOrders' => $totalAcceptedOrders,
+                    'total_confirmed_orders' => $totalConfirmedOrders,
+                    'total_supplied_orders' => $totalSuppliedOrders,
+                    'total_pending_orders' => $totalPendingOrders,
+                    'total_accepted_orders' => $totalAcceptedOrders,
                     'total_orders' => $totalOrders
                 ]
-            ]);
+            ], 200);
         }
 
-        return response()->json(['status' => false, 'message' => 'Vendor not found']);
+        return response()->json(['status' => false, 'message' => 'Vendor not found'], 404);
     }
 
 
