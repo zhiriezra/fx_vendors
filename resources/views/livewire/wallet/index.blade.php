f6e7b726a7c803c7f350c1573751adba19ed92c9
Alben-cmd <alenoghenabenjamin@gmail.com>
2024-11-10 20:51:04 +0100

    transaction export

--- resources/views/livewire/wallet/index.blade.php
+++ resources/views/livewire/wallet/index.blade.php
@@ -1,7 +1,4 @@
 <div>
-
-
-
     <div class="card">
         <div class="card-body">
 
@@ -13,5 +10,59 @@
             <button class="btn btn-outline-secondary" type="button" wire:click="requestWithdraw">Request Withdrawal</button>
           </div>
         </div>
+
+        
     </div>
+
+    <div class="card">
+      <div class="card-body">
+
+        <div class="text-lg">
+          <strong>Transaction History</strong>
+        </div>
+        <div class="card-body table-border-style">
+          <div class="datatable-top">
+            
+            <button wire:click="exportTransactions" class="btn btn-light-primary mb-1 btn-sm csv">
+                <span wire:loading.remove wire:target="exportTransactions">Export</span>
+                <span wire:loading wire:target="exportTransactions">
+                    <span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>
+                    Exporting...
+                </span>
+            </button>
+    </div>
+          <div class="table-responsive">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Transaction ID</th>
+                  <th>Amount</th>
+                  <th>Type</th>
+                  <th>Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                @forelse($transactions as $transaction)
+                <tr wire:key="{{$transaction->id }} ">
+                  <td>{{$loop->iteration }}</td>
+                  <td>{{$transaction->uuid }}</td>
+                  <td>&#8358;{{ number_format($transaction->amount,2) }}</td>
+                  <td>{{$transaction->type }}</td>
+                  <td>{{$transaction->created_at->toFormattedDateString() }}</td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="6"><h5 style="text-align: center;">0 Transactions</h5></td>
+                </tr>
+                @endforelse 
+              </tbody>
+            </table>
+          </div>
+        </div>
+        
+      </div>
+
+      
+  </div>
 </div>
