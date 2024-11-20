c5b06ae14c3b5d108c8dc4a91b60be4fd21ea673
zhiriezra <zhiriezra@gmail.com>
2024-11-20 16:32:48 +0100

    updated APIs for vendor profile update, location data

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -10,22 +10,51 @@ use Illuminate\Http\Request;
 class LocationController extends Controller
 {
     public function statesList(){
-        $states = State::all();
+
+        $allStates = State::all();
+        $states = [];
+        foreach($allStates as $state){
+            $states[] = ['id' => $state->id, 'name' => $state->name, 'active' => $state->active];
+        };
+
         return response()->json(['status' =>  true, 'message' => 'List of all states', 'data' => ['states' => $states]], 200);
     }
 
     public function state($id){
         $state = State::find($id);
-        return response()->json(['status' =>  true, 'message' => 'State', 'data' => ['state' => $state]], 200);
+        $lgas = Lga::where('state_id', $state->id)->get()->map(function ($lga){
+            return [
+                'id' => $lga->id,
+                'name' => $lga->name,
+                'active' => $lga->active
+            ];
+        });
+
+        $data = [
+            'state' => [
+                'id' => $state->id,
+                'name' => $state->name,
+                'active' => $state->active
+            ],
+
+            'lgas' => $lgas
+        ];
+        return response()->json(['status' =>  true, 'message' => 'State detail', 'data' => $data ], 200);
     }
 
     public function lgasList(){
-        $lgas = Lga::all();
+
+        $allLgas = Lga::all();
+        $lgas = [];
+        foreach($allLgas as $lga){
+            $lgas[] = ['id' => $lga->id, 'name' => $lga->name, 'active' => $lga->active];
+        };
+
         return response()->json(['status' =>  true, 'message' => 'List of all LGAs', 'data' => ['lgas' => $lgas]], 200);
     }
 
     public function lga($id){
         $lga = Lga::find($id);
-        return response()->json(['status' =>  true, 'message' => 'List of all LGAs', 'data' => ['lga' => $lga]], 200);
+        return response()->json(['status' =>  true, 'message' => 'LGA detail', 'data' => ['id' => $lga->id, 'name' => $lga->name, 'active' => $lga->active]], 200);
     }
 }
