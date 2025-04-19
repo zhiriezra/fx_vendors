157a7f170f911b74c6c34d031102e4b348756ac8
Alben-cmd <alenoghenabenjamin@gmail.com>
2025-04-19 21:54:40 +0100

    product fillable correction

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,7 +12,7 @@ class Product extends Model
     use HasFactory;
     use SoftDeletes;
 
-    protected $fillable = ['category_id', 'sub_category_id', 'manufacturer', 'name','quantity','unit_id','unit_price','agent_price', 'description', 'quantity', 'stock_date'];
+    protected $fillable = ['category_id', 'sub_category_id', 'manufacturer', 'name','quantity','unit_id','unit_price','agent_price', 'description', 'quantity', 'stock_date', 'vendor_id'];
 
     protected $casts = [
         'images' => 'array', // Cast images to an array
