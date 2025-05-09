c9c5183409ce2b0f48e6d57544536fac618dc8c1
Dakleo-e <dakleo.e@gmail.com>
2025-05-09 09:22:19 +0100

    updates

--- app/Models/State.php
+++ app/Models/State.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class State extends Model
 {
     use HasFactory;
+
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
+
+    public function lgas(){
+        return $this->hasMany(Lga::class);
+    }
+
+    public function agents()
+    {
+        return $this->hasMany(Agent::class);
+    }
+
+    public function vendors()
+    {
+        return $this->hasMany(Vendor::class);
+    }
+
+
 }
