916fce18f02aab1ecc457780d2cdf090e971cdfb
Alben-cmd <alenoghenabenjamin@gmail.com>
2025-05-10 21:46:49 +0100

    order Export

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -13,33 +13,43 @@ class OrdersExport implements FromCollection, WithHeadings
     /**
     * @return \Illuminate\Support\Collection
     */
+    
     public function collection()
     {
         return Order::join('products', 'orders.product_id', '=', 'products.id')
-        ->join('vendors', 'products.vendor_id', '=', 'vendors.id')
-        ->join('users as vendor_users', 'vendors.user_id', '=', 'vendor_users.id') // For Vendor's User Info
-        ->join('farmers', 'orders.farmer_id', '=', 'farmers.id') // For Farmer's Info
-        ->join('agents', 'orders.agent_id', '=', 'agents.id')
-        ->join('users as agent_users', 'agents.user_id', '=', 'agent_users.id') // For Agent's User Info
-        ->where('vendors.user_id', auth()->id()) // Filter by Auth user
-        ->select(
-            'products.name as product_name',
-            DB::raw("CONCAT(farmers.fname, ' ', farmers.lname) as farmer_fullname"),
-            DB::raw("CONCAT(agent_users.firstname, ' ', agent_users.lastname) as agent_fullname"),
-            'orders.quantity',
-            'orders.status'
-        )
-        ->get();
+            ->join('transactions', 'orders.transaction_id', '=', 'transactions.id')
+            ->join('vendors', 'products.vendor_id', '=', 'vendors.id')
+            ->join('users as vendor_users', 'vendors.user_id', '=', 'vendor_users.id')
+            ->join('farmers', 'orders.farmer_id', '=', 'farmers.id')
+            ->join('agents', 'orders.agent_id', '=', 'agents.id')
+            ->join('users as agent_users', 'agents.user_id', '=', 'agent_users.id')
+            ->where('vendors.user_id', auth()->id())
+            ->select(
+                'transactions.uuid as transaction_uuid', // Include UUID from transactions
+                'products.name as product_name',
+                DB::raw("CONCAT(farmers.fname, ' ', farmers.lname) as farmer_fullname"),
+                DB::raw("CONCAT(agent_users.firstname, ' ', agent_users.lastname) as agent_fullname"),
+                'orders.quantity',
+                'orders.unit_price',
+                'orders.status',
+                'orders.created_at',
+                'orders.updated_at'
+            )
+            ->get();
     }
 
     public function headings(): array
     {
         return [
+            'Transaction UUID', 
             'Product Name',
             'Farmer Name',
             'Agent Name',
             'Quantity',
+            'Unit Price',
             'Status',
+            'Created At',
+            'Updated At'
         ];
     }
 }
