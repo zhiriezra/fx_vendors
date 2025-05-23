efce23138bb599e8be7aa3718010855cce159e9e
Dakleo-e <dakleo.e@gmail.com>
2025-05-23 08:00:46 +0100

    updates

--- app/Models/Escrow.php
+++ app/Models/Escrow.php
@@ -11,8 +11,28 @@ class Escrow extends Model
 
     protected $guarded = [''];
 
-    /* public function product()
+    public function agent()
+    {
+        return $this->belongsTo(Agent::class);
+    }
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+
+    public function product()
     {
         return $this->belongsTo(Product::class, 'product_id');
-    } */
+    }
+
+    public function order_processings()
+    {
+        return $this->hasMany(OrderProcessing::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
