dd90de0c1d03cc939e273b38cdf6cd418304b693
Alben-cmd <alenoghenabenjamin@gmail.com>
2025-05-23 09:12:13 +0100

    transaction correction

--- app/Exports/TransactionsExport.php
+++ app/Exports/TransactionsExport.php
@@ -5,7 +5,6 @@ namespace App\Exports;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\FromCollection;
