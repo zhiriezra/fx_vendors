c1fb800a89ae5dbebe90075d69d54844b0c566c3
zhiriezra <zhiriezra@gmail.com>
2025-05-25 18:10:38 +0100

    testing the app

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
+use App\Models\StockTracker;
 
 class Product extends Model
 {
